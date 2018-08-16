57188d2a0c127ef2659dc32da8eed0c6ec7a3474
™ധู้ざຣ้ণს✚ປิʨℓℓҨබମ™ <39564544+0954517662@users.noreply.github.com>
2018-08-16 16:21:09 +0700

    Update max5.py

--- max5.py
+++ max5.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-
-from gyevha import *
+from linepy import *
+from akad.ttypes import *
+from multiprocessing import Pool, Process
+from akad.ttypes import ContentType as Type
 from datetime import datetime
-from time import sleep
+import time,random,sys,json,codecs,threading,glob,re,os,subprocess
 from bs4 import BeautifulSoup
 from humanfriendly import format_timespan, format_size, format_number, format_length
-import time, random, sys, json, codecs, threading, glob, re, string, os, requests, subprocess, six, ast, pytz, urllib, urllib.parse
+import time, random, sys, json, codecs, threading, glob, re, string, os, requests, subprocess, six, ast, pytz, urllib, urllib.parse,youtube_dl,pafy,timeit,atexit,traceback
 from gtts import gTTS
 from googletrans import Translator
  
