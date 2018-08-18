50f1bf06f08b581dc00b3f989424ed53416ec797
™ധู้ざຣ้ণს✚ປิʨℓℓҨබମ™ <39564544+0954517662@users.noreply.github.com>
2018-08-18 16:45:35 +0700

    Update maxx.py

--- maxx.py
+++ maxx.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 from linepy import *
+from akad.ttypes import *
+from multiprocessing import Pool, Process
+from akad.ttypes import ContentType as Type
 from datetime import datetime
-from time import sleep
+import time,random,sys,json,codecs,threading,glob,re,os,subprocess
 from bs4 import BeautifulSoup
 from humanfriendly import format_timespan, format_size, format_number, format_length
-import time, random, sys, json, codecs, threading, glob, re, string, os, requests, subprocess, six, ast, pytz, urllib, urllib.parse
+import time, random, sys, json, codecs, threading, glob, re, string, os, requests, subprocess, six, ast, pytz, urllib, urllib.parse,youtube_dl,pafy,timeit,atexit,traceback
 from gtts import gTTS
+from googletrans import Translator
 #=================================================================#
 ### LOGIN SETTING DISINI ###
 arif = LINE()##LOGIN LEWAT TOKEN
